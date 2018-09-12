c32ae024b096f3f25132e7d5ff9e0ec8843eec6d
jerielng <jeriel.ng@gmail.com>
2018-09-11 23:56:30 -0500

    Fixed sigmoid, implemented forward pass

--- my_answers.py
+++ my_answers.py
@@ -17,7 +17,7 @@ class NeuralNetwork(object):
         self.lr = learning_rate
         
         def sigmoid(x):
-           return (1 / (1 + np.exp(-1)))
+           return (1 / (1 + np.exp(-x)))
         self.activation_function = sigmoid
                     
 
@@ -54,12 +54,12 @@ class NeuralNetwork(object):
         #### Implement the forward pass here ####
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(self.input_nodes, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(X) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+        final_outputs = final_inputs # signals from final output layer
         
         return final_outputs, hidden_outputs
 
